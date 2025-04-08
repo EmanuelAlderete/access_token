7a5f966ef8209e42f4a28ed375634fb07b287d36
Emanuel Alderete <luisemanuel.alderete@gmail.com>
2025-04-08 16:33:37 -0300

    fix: autorizado acesso ao painel administrativo

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,7 +53,7 @@ class User extends Authenticatable
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@admin.com');
+        return true;
     }
 
     public function tokens(): MorphMany
